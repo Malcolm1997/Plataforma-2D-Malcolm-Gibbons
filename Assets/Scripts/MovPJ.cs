08b36b7d991a9e747933e0b1abdb83b1074cae48
Malcolm Alfredo Gibbons <malcolm.gibbons1997@gmail.com>
2021-05-03 00:06:08 -0300

    Trabajo finalizado

---
+++ Assets/Scripts/MovPJ.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovPJ : MonoBehaviour
+{
+    public GameObject PJ;
+    float mov = 5f;
+    float salto = 8f;
+    Rigidbody rb;
+    bool piso = true;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            PJ.transform.position = PJ.transform.position + new Vector3(-mov * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            PJ.transform.position = PJ.transform.position + new Vector3(mov * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if(piso == true) {
+                rb.velocity =Vector2.up * salto;
+                piso = false;
+            }
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Piso"))
+        {
+            piso = true;
+        }
+        if (collision.gameObject.CompareTag("DangerZone"))
+        {
+            PJ.transform.position = new Vector3(-8, -2, 0);
+        }
+        if (collision.gameObject.CompareTag("Win"))
+        {
+            Debug.Log("Gane");
+        }
+    }
+}
